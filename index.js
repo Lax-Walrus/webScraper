8a1601caac3af4c8e7a1aaebd7aaa9a1fd4d7a29
Kevin Lyons <zinckev@gmail.com>
2021-10-25 12:16:58 -0700

    commiting more to leave a trail

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const express = require("express");
+const cheerio = require("cheerio");
+const PORT = 8080;
+
+const app = express();
+
+axios("https://www.cnn.com/")
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+    $(`.cd__headline`, html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      console.log(title);
+      articles.push({ title, url });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
